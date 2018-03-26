cc40f67a407924d40018779559c5bf7aad18ef12
Kite's Runner <shengyao@umich.edu>
2018-03-25 21:19:59 -0400

    tune

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -54,14 +54,15 @@ class FG_eval {
 
     // Minimize the use of actuators.
     for (unsigned int t = 0; t < N - 1; t++){
-      fg[0] += 10*CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 100*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += 10*CppAD::pow(vars[a_start + t], 2);
-      fg[0] += 600*CppAD::pow(vars[delta_start + t] * vars[v_start + t], 2);
+      // TO avoid the swing, use suggestion from the forum.
+      fg[0] += 500*CppAD::pow(vars[delta_start + t] * vars[v_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuatotions.
     for (unsigned int t = 0; t < N -2; t++){
-      fg[0] += 100*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 10*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
     //
