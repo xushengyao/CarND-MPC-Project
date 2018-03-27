7358433a63d90414b632754530a74458914afa9f
Kite's Runner <shengyao@umich.edu>
2018-03-26 21:06:52 -0400

    tune

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -139,7 +139,7 @@ MPC::~MPC() {}
 
 vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   bool ok = true;
-  const size_t i;
+  size_t i;
   typedef CPPAD_TESTVECTOR(double) Dvector;
 
   double x = state[0];
