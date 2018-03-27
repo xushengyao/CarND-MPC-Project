89e0df1ee920b7ad3dcad8cf035f27b975fdac2d
Kite's Runner <shengyao@umich.edu>
2018-03-26 22:04:36 -0400

    tune

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -47,7 +47,7 @@ class FG_eval {
 
     // The part of the cost based on the reference state.
     for (unsigned int t = 0; t < N; t++) {
-      fg[0] += 10*CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 30*CppAD::pow(vars[cte_start + t], 2);
       fg[0] += 150*CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
