0c468c60d36b93c95098c1ebf96058a60cc61694
Kite's Runner <shengyao@umich.edu>
2018-03-26 23:29:24 -0400

    t

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -47,14 +47,14 @@ class FG_eval {
 
     // The part of the cost based on the reference state.
     for (unsigned int t = 0; t < N; t++) {
-      fg[0] += 5000*CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 1000*CppAD::pow(vars[cte_start + t], 2);
       fg[0] += 2500*CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
     for (unsigned int t = 0; t < N - 1; t++){
-      fg[0] += 1500*CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 1000*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t], 2);
       // TO avoid the swing, use suggestion from the forum.
      //fg[0] += 100*CppAD::pow(vars[delta_start + t] * vars[v_start + t], 2);
