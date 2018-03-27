56da3a0d4bee6eb0beae8cc176c636fdfc997d0a
Kite's Runner <shengyao@umich.edu>
2018-03-26 21:54:50 -0400

    tune

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -62,7 +62,7 @@ class FG_eval {
 
     // Minimize the value gap between sequential actuatotions.
     for (unsigned int t = 0; t < N -2; t++){
-      fg[0] += 500*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 100*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
